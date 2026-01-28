fc5a0e3b34dd69fa97e702484b16bd499eb272d2
Shelia Sportsman <ssportsm@student.scf.edu>
2026-01-28 15:22:54 -0500

    Add files via upload

--- doc_string_converter.py
+++ doc_string_converter.py
@@ -1,16 +1,16 @@
 import inspect
-import SheliaSportsman_Docstring_Exercise_1  # replace with your assignment name (without .py)
+import Shelia_Sportsman_Debugging_Exercise  # replace with your assignment name (without .py)
 
 #replace docstring_example with your assignment name in the next 2 lines of code
-with open("SheliaSportsman_Docstring_Exercise_1_design_doc.txt", "w") as doc:
-    doc.write(f"# Technical Design Document: {SheliaSportsman_Docstring_Exercise_1.__name__}\n\n")
+with open("Debugging_Exercise_design_doc.txt", "w") as doc:
+    doc.write(f"# Technical Design Document: {Debugging_Exercise.__name__}\n\n")
     #replace with your name, the date, and the description of the program
     doc.write(f"# Name: Shelia Sportsman\n")
-    doc.write(f"# Date: January 21, 2026\n")
+    doc.write(f"# Date: January 28, 2026\n")
     doc.write(f"# Program Description: Give brief description of your program\n\n")
 
     #replace docstring_example with your assignment name 
-    for name, func in inspect.getmembers(SheliaSportsman_Docstring_Exercise_1, inspect.isfunction):
+    for name, func in inspect.getmembers(Shelia_Sportsman_Debugging_Exercise, inspect.isfunction):
         doc.write(f"## Function: {name}\n")
         doc.write(f"{inspect.getdoc(func)}\n\n")
     
