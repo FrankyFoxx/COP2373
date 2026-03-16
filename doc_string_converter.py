7a57a2924c482aede3b29788b0edc77b363ac671
Shelia Sportsman <ssportsm@student.scf.edu>
2026-03-16 14:56:06 -0400

    Add files via upload

--- doc_string_converter.py
+++ doc_string_converter.py
@@ -6,7 +6,7 @@ with open("SheliaSportsman_Prog_Exercise07.txt", "w") as doc:
     # Header information
     doc.write("# Technical Design Document: SheliaSportsman_Prog_Exercise07n\n")
     doc.write("Name: Shelia Sportsman\n")
-    doc.write("Date: March 08, 2026\n")
+    doc.write("Date: March 016, 2026\n")
     doc.write("Program Description: Give brief description of your program\n\n")
 
     # Loop through functions in the module
